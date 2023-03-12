cf5a377c6771c89a7cbfbe733fedfa18b165af1d
ago25cdh <ago25cdh@icloud.com>
2023-03-13 00:09:47 +0900

    Initial commit

---
+++ src/main/java/me/dhcha/App.java
@@ -0,0 +1,29 @@
+package me.dhcha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+/*
+        Training git
+        1. install git
+            installed
+        2. brew info git
+        3. sync idle-github setting - settings - Version Control -github
+        4.
+
+
+
+
+
+
+
+
+
+*/
+    }
+}
